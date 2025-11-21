a23681883bb033e0633e6b39c12258aaa093870d
makukudenbar <makukudenbar1@gmail.com>
2025-11-21 14:51:35 +0200

    Add basic arithmetic operations script

---
+++ Arithmetic operations.py
@@ -0,0 +1,7 @@
+Arithmetic operations.py
+>>> python .\main.py
+Arithmetic Operations
+Enter the first number: 5
+Enter the second number: 6
+Enter the operation (add, subtract, multiply, divide): add
+Result: 11.0
