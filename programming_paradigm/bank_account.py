e1e4aea9197489fc03d0b681b89c4c62b06e9854
Denbar Makuku <makukudenbar1@gmail.com>
2025-11-27 17:46:51 +0200

    bankaccount class implementation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}.00")
+        print(f"Current Balance: ${self.account_balance}.00")
 
 #class BankAccount:
 #     def __init__(self, initial_balance=0):
