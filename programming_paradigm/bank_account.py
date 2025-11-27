266c7a3912de237cccbeb8cc8fb32d421a1217e7
Denbar Makuku <makukudenbar1@gmail.com>
2025-11-27 17:43:15 +0200

    bankaccount class implementation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,10 +3,33 @@ class BankAccount:
         self.account_balance = initial_balance
     
     def deposit(self, amount):
-        self.account_balance += amount
+        if amount > 0:
+            self.account_balance += amount
+            print(f"Deposited: ${amount}. New balance: ${self.account_balance}")
+        else:
+            print("Deposit amount must be positive.")
     
     def withdraw(self, amount):
-        self.account_balance -= amount
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        elif amount > self.account_balance:
+            return False
+        else:
+            return False
     
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current balance: ${self.account_balance}")
+
+#class BankAccount:
+#     def __init__(self, initial_balance=0):
+#         self.account_balance = initial_balance
+    
+#     def deposit(self, amount):
+#         self.account_balance += amount
+    
+#     def withdraw(self, amount):
+#         self.account_balance -= amount
+    
+#     def display_balance(self):
+#         print(f"Current Balance: ${self.account_balance}")
