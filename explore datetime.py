3ebb28ec67f652e6e4e4e7317f621c0fb59285f3
makukudenbar <makukudenbar1@gmail.com>
2025-12-11 20:53:44 +0200

    added oop project

--- explore datetime.py
+++
@@ -1,3 +0,0 @@
-Current date and time: 2024-03-25 15:30:45
-Enter the number of days to add to the current date: 10
-Future date: 2024-04-04
--- temp conversion tool.py
+++
@@ -1,3 +0,0 @@
-Enter the temperature to convert: 0
-Is this temperature in Celsius or Fahrenheit? (C/F): C
-0.0°C is 32.0°F
